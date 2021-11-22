7c8025e4df7eaef3e24e312b4228993191c25321
CommoZ <51430113+CommoZ@users.noreply.github.com>
2021-11-23 06:17:43 +0800

    how bout a random commit

--- Test Server/Program.cs
+++ Test Server/Program.cs
@@ -4,6 +4,8 @@ namespace Test_Server
 {
 	class Program
 	{
+		//This random commit is not the only thing i want to commit
+
 		static void Main(string[] args)
 		{
 			Console.Title = "Game Server";
